9d85ef1393a5a6738c1bbd19056aca679867bafc
Sikander kanojia <sikanderkanojia1511@gmail.com>
2026-03-31 10:06:00 +0530

    fix: dynamically set the base URL for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/Linux-tools/",
+export default defineConfig(({ mode, command }) => ({
+  base: command === "serve" ? "/" : "/Linux-tools/",
   server: {
     host: "::",
     port: 8080,
